341aaff644889210b00baeaf1947f12d5c7875f4
Ethan Edwards-Jones <ethan@DESKTOP-TIOIRRU.localdomain>
2026-02-04 12:28:07 +0000

    stated to implement the other methods required for the global planner - created in header but need to construct in .cpp file

--- src/f1tenth_system/global_planner/include/global_planner/tta_planner.hpp
+++ src/f1tenth_system/global_planner/include/global_planner/tta_planner.hpp
@@ -15,4 +15,47 @@ public:
     const std::vector<BoundaryPoint> & right_boundary,
     std::vector<BoundaryPoint> & centerline
   );
+
+
+private:
+  std::vector<BoundaryPoint> preprocessBounderies(
+    const std::vector<BoundaryPoint> & boundary
+  );
+
+  std::vector<BoundaryPoint> orderBoundary(
+    const std::vector<BoundaryPoint> & boundary
+  );
+
+  std::vector<std::pair<BoundaryPoint, BoundaryPoint>> pairBoundaryPoints(
+    const std::vector<BoundaryPoint> & left_chain,
+    const std::vector<BoundaryPoint> & right_chain
+  );
+
+  std::vector<BoundaryPoint> computeMidpoints(
+    const std::vector<std::pair<BoundaryPoint, BoundaryPoint>> & pairs
+  );
+
+  std::vector<BoundaryPoint> orderLoop(
+    const std::vector<BoundaryPoint> & centerline
+  );
+
+  std::vector<BoundaryPoint> smooth(
+    const std::vector<BoundaryPoint> & ordered_centerline
+  );
+
 };
+
+
+
+
+
+/*
+preprocessBounderies();
+    left_chain = orderBoundary(left_boundary);
+    right_chain = orderBoundary(right_boundary);
+    pairs = pairBoundaryPoints(left_chain, right_chain);
+    centerline = computeMidpoints(pairs);
+    ordered = oderLoop(centerline);
+    centerline_out = smoot(ordered);
+
+*/
