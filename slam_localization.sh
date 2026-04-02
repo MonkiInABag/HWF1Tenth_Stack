abaaa9b5d841c2445bb388321fd82978a2ffb5f9
Szymon-Ilnicki <szymon@DESKTOP-8FBHHLF.localdomain>
2026-04-02 22:07:35 +0100

    useful shortcuts

---
+++ slam_localization.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/szymon_ws/HWF1Tenth_Stack
+source install/setup.bash
+echo "=== Starting SLAM Localization ==="
+ros2 launch slam_toolbox online_async_launch.py \
+  slam_params_file:=src/f1tenth_system/f1tenth_stack/config/tiny_mapper_params_localization.yaml
